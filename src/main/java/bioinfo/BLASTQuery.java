5f56893655f25bc3c538ce2571d82848fe63b1eb
Federico T <ftedinur@itba.edu.ar>
2015-11-18 17:41:16 -0300

    add biojava-ws to pom.xml

--- src/main/java/bioinfo/BLASTQuery.java
+++ src/main/java/bioinfo/BLASTQuery.java
@@ -1,5 +1,10 @@
 package bioinfo;
 
+import static org.biojava.nbio.ws.alignment.qblast.BlastAlignmentParameterEnum.ENTREZ_QUERY;
+import java.io.*;
+import org.biojava.nbio.core.sequence.io.util.IOUtils;
+import org.biojava.nbio.ws.alignment.qblast.*;
+
 public class BLASTQuery {
 	public static void main(String[] args) {
 		
