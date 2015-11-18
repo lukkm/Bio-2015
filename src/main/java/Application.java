6c5ce5c0376296052f462189789b5c271e2b4ed9
Lucas Moscovicz <lmoscovicz@ElLukoHotspot.roja.itba.edu.ar>
2015-11-18 17:02:19 -0300

    Read Genbank Files

---
+++ src/main/java/Application.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.util.LinkedHashMap;
+
+public class Application {
+
+	public static void main(String[] args) {
+		/*
+		 * Method 2: With the GenbankReaderHelper
+		 */
+		//Try with the GenbankReaderHelper
+		File dnaFile = new File("src/test/resources/NM_000266.gb");		
+		File protFile = new File("src/test/resources/BondFeature.gb");
+		
+
+	}
+	
+}
